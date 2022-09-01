2f04c6f06cc631ee0d3b6f25d31646ca7ce77a8e
Egor <egormaster543@gmail.com>
2022-09-01 18:46:56 +0300

    second project

--- 22.py
+++ 22.py
@@ -1,11 +1,12 @@
 # put your python code here
 a = int(input())
+b = int(input())
 if a <= 13:
-    print('детство')
+    print('детство',b)
 elif 14 <= a <= 24:
-    print('молодость')
+    print('молодость',b)
 elif 25 <= a <= 59:
-    print('зрелость')
+    print('зрелость',b)
 elif 60 <= a:
-    print('старость')
+    print('старость',b)
 
