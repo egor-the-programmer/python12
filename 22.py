266a21b37ad9c47a4ed7fbbcaee0e255642dd359
Egor <egormaster543@gmail.com>
2022-09-01 18:44:45 +0300

    second project

---
+++ 22.py
@@ -0,0 +1,11 @@
+# put your python code here
+a = int(input())
+if a <= 13:
+    print('детство')
+elif 14 <= a <= 24:
+    print('молодость')
+elif 25 <= a <= 59:
+    print('зрелость')
+elif 60 <= a:
+    print('старость')
+
